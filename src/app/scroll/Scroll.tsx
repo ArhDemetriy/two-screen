11a3e7daeb8f8848b47b3f74be21e9fcf13bea4b
Demetriy <ArhDemetriy@GMail.com>
2021-05-20 20:09:04 +0800

    App dane

--- src/app/scroll/Scroll.tsx
+++ src/app/scroll/Scroll.tsx
@@ -3,30 +3,22 @@ import './Scroll.scss'
 import Notepad from './notepad/Notepad';
 import Clock from './clock/Clock';
 
-class Scroll extends React.Component<{}, { translate: number }> {
-  constructor(props?: any) {
-    super(props);
-    this.state = { translate: 0 };
-  }
-  componentDidMount() {
-  }
-  componentWillUnmount() {
-  }
-  render() {
-    return (
-      <div className="Scroll" style={{ '--translate': this.state.translate } as CSSProperties}>
-        <div className="Scroll__page">
-          <Notepad/>
-        </div>
-        <div className="Scroll__page">
-          <Clock/>
-        </div>
-      </div>
-    );
-  }
-  // values
-  // methods
+export interface ScrollProps{
+  page: 'first' | 'second'
+}
 
+function Scroll({ page }: ScrollProps) {
+  return (
+    <div className="Scroll"
+      style={{ '--translate': page == 'second' ? -100 : 0 } as CSSProperties}>
+      <div className="Scroll__page">
+        <Notepad/>
+      </div>
+      <div className="Scroll__page">
+        <Clock/>
+      </div>
+    </div>
+  );
 }
 
 export default Scroll;
