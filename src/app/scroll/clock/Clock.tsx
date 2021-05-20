f52a35f9116d41d7255939dd48d4a00f45861f5f
Demetriy <ArhDemetriy@GMail.com>
2021-05-20 16:54:24 +0800

    add Clock

--- src/app/scroll/clock/Clock.tsx
+++ src/app/scroll/clock/Clock.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import './Clock.scss'
 
-function Clock() {
-  return (
-    <div className="Clock">
-      "clock"
-    </div>
-  );
+class Clock extends React.Component<{}, {date: Date}> {
+  constructor(props?: any) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  componentDidMount() {
+    this.timerID = setInterval(() => this.setState({ date: new Date() }), 1000);
+  }
+  componentWillUnmount() {
+    if (this.timerID) {
+      clearInterval(this.timerID);
+    }
+  }
+  render() {
+    return (
+      <span>
+        {this.state.date.toLocaleTimeString()}
+      </span>
+    );
+  }
+  // values
+  protected timerID?: NodeJS.Timeout
 }
 
 export default Clock;
