8a438a6378add7a5aa3f7bd3e8d55c114afed826
Demetriy <ArhDemetriy@GMail.com>
2021-05-20 18:46:47 +0800

    Notepad dane

--- src/app/scroll/notepad/Notepad.tsx
+++ src/app/scroll/notepad/Notepad.tsx
@@ -3,13 +3,34 @@ import './Notepad.scss'
 import TextList from "./textList/TextList";
 import Input from "./input/Input";
 
-function Notepad() {
-  return (
-    <form className="Notepad">
-      <Input></Input>
-      <TextList></TextList>
-    </form>
-  );
+class Notepad extends React.Component<{}, { textList: { id: number, text: string }[] }> {
+  constructor(props?: any) {
+    super(props);
+    this.state = { textList: [] };
+  }
+  componentDidMount() {
+  }
+  componentWillUnmount() {
+  }
+  render() {
+    return (
+      <div className="Notepad">
+        <Input pusher={this.pushText.bind(this) as Notepad['pushText']}/>
+        {this.state.textList.map(({ text, id }) => <span key={id}>{text}</span>)}
+      </div>
+    );
+  }
+  // values
+  // methods
+  protected pushText(text: string) {
+    const textListItem = {
+      text,
+      id: Math.random()
+    }
+    this.state.textList.push(textListItem)
+    this.setState(this.state)
+  }
+
 }
 
 export default Notepad;
--- src/app/scroll/notepad/input/Input.tsx
+++ src/app/scroll/notepad/input/Input.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './Input.scss'
-
-function Input() {
+interface InputProps{
+  pusher: (text: string) => void
+}
+function Input({ pusher }: InputProps) {
   return (
-    <label className="Input">
+    <form className="Input" onSubmit={ev => {
+      ev.preventDefault();
+      const textField = (ev.currentTarget as HTMLFormElement).firstChild as HTMLInputElement
+      if (textField && textField.value) {
+        pusher(textField.value);
+      }
+    }}>
       <input className='Input__textField' type="text" name="textField" />
-      <button type="button">Добавить текст</button>
+      <button type="submit">Добавить текст</button>
 
-    </label>
+    </form>
   );
 }
 
